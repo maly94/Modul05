7f5366245d84c4281e52457d9a74f91f75dfccb7
maly94 <kamil.maly@vp.pl>
2016-04-25 13:39:05 +0200

    wkurzyles mnie

--- Modul05/Macierze/Program.cs
+++ Modul05/Macierze/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Macierze //dsfsdfdsfds kos
+namespace Macierze //dsfsdfdsfds kos gdfgdf
 {
     class Program
     {
