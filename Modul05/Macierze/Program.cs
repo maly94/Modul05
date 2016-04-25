5d2d596b9811cb0c38cd3964d9cc912126d2f3b5
pawelgorny94 <pawelgrony94@gmail.com>
2016-04-25 13:43:14 +0200

    fhoigy

--- Modul05/Macierze/Program.cs
+++ Modul05/Macierze/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Macierze //dsfsdfdsfds kos gdfgdfe ewfwefewf
+namespace Macierze //dsfsdfdsfds kos gfgjffe ewfwefewf
 {
     class Program
     {
