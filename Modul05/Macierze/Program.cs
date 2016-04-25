aa869be1608105a8fb13b92802ed7df5b73c262e
maly94 <kamil.maly@vp.pl>
2016-04-25 13:42:34 +0200

    fddfd

--- Modul05/Macierze/Program.cs
+++ Modul05/Macierze/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Macierze //dsfsdfdsfds kos gdfgdfe ewfwefewf
+namespace Macierze //dsfsddsfvds
 {
     class Program
     {
